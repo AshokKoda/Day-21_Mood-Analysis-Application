33b22bab0e7b6500940535d34c21ed98d19c7b93
Ashok Koda <a.koda59@gmail.com>
2021-12-23 16:18:43 +0530

    Added: Mood analysis Happy or Sad

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+
+	public String moodAnalysis(String msg) {
+
+		if (msg.contains("sad")) {
+			return "Sad";
+		} else {
+			return "Happy";
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		MoodAnalyser moodAnalysis = new MoodAnalyser();
+		String mood = moodAnalysis.moodAnalysis("Iam sad mood");
+		System.out.println("My mood is: " + mood);
+
+	}
+
+}
